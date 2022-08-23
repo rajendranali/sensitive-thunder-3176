9375c4a18bb1a53768a47d444a93a7e6c38160fa
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-08-23 19:09:52 +0530

    routes in folder

--- sensitive-thunder-3176/src/Pages/Allroutes.jsx
+++ sensitive-thunder-3176/src/Pages/Allroutes.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Blog from "./Blog";
 import Bookdemo from "./Bookdemo";
-import Feature from "./Feature";
-import Integration from "./Integration";
-import Pricing from "./Pricing";
-import Signin from "./Signin";
+import Feature from "./Feature/Feature";
+import Home from "./Home/Home";
+import Integration from "./Integration/Integration";
+import Pricing from "./Pricing/Pricing";
+import Signin from "./Signin/Signin";
 
 const Allroutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home/>}></Route>
       <Route path="/feature" element={<Feature/>}></Route>
       <Route path="/pricing" element={<Pricing/>}></Route>
       <Route path="/integration" element={<Integration/>}></Route>
