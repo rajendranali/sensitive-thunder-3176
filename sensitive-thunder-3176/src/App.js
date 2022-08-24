fca43d78870fa0d94b685056194a1ff6b557b8f4
Rajendra Nali <80375569+rajendranali@users.noreply.github.com>
2022-08-24 21:13:53 +0530

    allintegration day-2

--- sensitive-thunder-3176/src/App.js
+++ sensitive-thunder-3176/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Allroutes from './Pages/Allroutes';
+import Integration from './Pages/Integration/Integration';
 
 function App() {
   return (
     <div className="App">
 <Allroutes/>
+<Integration/>
     </div>
   );
 }
