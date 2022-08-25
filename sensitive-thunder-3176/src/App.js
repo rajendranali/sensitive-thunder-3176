873595aed6386a986e1f9262c71676af501cae5b
Rajendra Nali <80375569+rajendranali@users.noreply.github.com>
2022-08-25 23:54:43 +0530

    user

--- sensitive-thunder-3176/src/App.js
+++ sensitive-thunder-3176/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 import Allroutes from './Pages/Allroutes';
 import Integration from './Pages/Integration/Integration';
+import User from './Pages/User/User';
 
 function App() {
   return (
     <div className="App">
 
 <Allroutes/>
-
+<User/>
     </div>
   );
 }
---
+++ sensitive-thunder-3176/src/Pages/User/User.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Box,Heading,Text,Flex,Button} from "@chakra-ui/react"
+import {FiUserPlus} from "react-icons/fi"
+
+import {Link} from "react-router-dom"
+import styles from "./user.module.css"
+import GuestInvite from './modal/GuestInvite'
+function User() {
+  return (
+    <div>
+        <Box>
+            <Box w="900px" h="50px" backgroundColor="#d9e9fe" justifyContent={"center"} borderRadius="10px" paddingTop={1}>
+               <Flex> <FiUserPlus   className={styles.user}/> <Text marginTop="auto"color="#88a9f9" marginLeft="10px" >You can now invite guests like clients, vendors, or contractors. You can add them free of charge</Text>
+               <a href="" target="_blank" style={{marginLeft:"2px",color:"#88a9f9" ,fontWeight:"700",textDecoration: "underline",marginTop:"auto"}}>.Invite Guest</a></Flex>
+            </Box>
+           <Box>
+           <Flex>
+           
+            <GuestInvite/>
+            <GuestInvite/>
+            <GuestInvite/>
+           
+           </Flex>
+           </Box>
+        </Box>
+        
+    </div>
+  )
+}
+
+export default User
