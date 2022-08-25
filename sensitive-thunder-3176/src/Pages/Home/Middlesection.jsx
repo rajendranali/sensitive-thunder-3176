aa7abe009dfc189a123c270f5ec89c8f9ea7926a
Mohammadzaki0027 <zaki7875@gmail.com>
2022-08-25 17:24:51 +0530

    day-3part1

--- sensitive-thunder-3176/src/Pages/Home/Middlesection.jsx
+++ sensitive-thunder-3176/src/Pages/Home/Middlesection.jsx
@@ -1,27 +1,67 @@
-import React from 'react'
-import a from "../../Styles/Home/home.module.css"
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import a from "../../Styles/Home/home.module.css";
 const Middlesection = () => {
   return (
     <div className={a.image_container}>
-  
-    <div className={a.image_div}>
-<div className={a.above_imagediv}>
+      <div className={a.image_div}>
+        <div className={a.above_imagediv}>
+          <div className={a.happy_box}>
+            <div className={a.inner_div}>
+              <div className={a.text_div1}>
+                <h1>Free time tracking software</h1>
+              </div>
+              <div className={a.text_div}>
+                <h2>Happy to see you</h2>
+              </div>
+              <div className={a.text_div}>
+                <h2>again!</h2>
+              </div>
+              <div className={a.text_div} >
+                <h2>Book TimeCamp</h2>
+              </div>
+              <div className={a.text_div}>
+                <h2>set-up call</h2>
+              </div>
+              <div className={a.text_div1} >
+                <h1 >✓ Showing how TimeCamp works in 15 minutes</h1>
+              </div>
+              <div className={a.text_div1}>
+                <h1>✓ Best practices from 5000+ implementations</h1>
+              </div>
+              <div className={a.text_div1}>
+                <h1>✓ Help with dedicated setup</h1>
+              </div>
+            <div className={a.btn_reveiw}>
+              <div><Button className={a.btn}>Book a demo</Button></div>
+              <h4 className={a.text}>or</h4>
+              <div><Button className={a.btn}>Go to App</Button></div>
+            
+            
+            </div>
+            </div>
+          </div>
+          <div className={a.outer}>
+            <div className={a.circle_image}></div>
+            <div className={a.review_company}>
+              <h2>Adam Wagner, Co-owner and Chief Strategy Officer</h2>
+              <p>
+                „TimeCamp proved to be an essential management tool that gives
+                us clear insight into a project’s efficiency and helps to avoid
+                an overblown workload for our team.”
+              </p>
 
-  <div className={a.happy_box}></div>
-  <div className={a.outer}>
-<div className={a.circle_image}></div> 
- <div className={a.review_company}> 
-<h2 >Adam Wagner, Co-owner and Chief Strategy Officer</h2>
-<p>„TimeCamp proved to be an essential management tool that gives us clear insight into a project’s efficiency and helps to avoid an overblown workload for our team.”</p>
-
-<img src="https://cdn-m.timecamp.com/img/raindrop_logo.svg" alt="" className={a.logo_raindroup} />
-  </div> 
-  </div>
-
-</div>
+              <img
+                src="https://cdn-m.timecamp.com/img/raindrop_logo.svg"
+                alt=""
+                className={a.logo_raindroup}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Middlesection
+export default Middlesection;
