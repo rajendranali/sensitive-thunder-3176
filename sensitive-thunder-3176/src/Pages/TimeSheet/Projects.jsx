25f6cd17fd03b51699835cff802a88f1c8cc3dbc
Mohammadzaki0027 <zaki7875@gmail.com>
2022-08-27 18:05:53 +0530

    pr

--- sensitive-thunder-3176/src/Pages/TimeSheet/Projects.jsx
+++ sensitive-thunder-3176/src/Pages/TimeSheet/Projects.jsx
@@ -1,158 +1,76 @@
-import { Button, IconButton } from "@chakra-ui/button";
-import { Image } from "@chakra-ui/image";
-import { Input } from "@chakra-ui/input";
-import { Box, Flex, HStack, Spacer, Text, VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
-import { Tooltip } from "@chakra-ui/tooltip";
+import { Input } from "@chakra-ui/input";
+import { Box, Text } from "@chakra-ui/layout";
+
 import React, { useEffect } from "react";
-import { useState } from "react";
-import {
-  FiCalendar,
-  FiChevronDown,
-  FiChevronLeft,
-  FiChevronRight,
-  FiCopy,
-  FiMoreVertical,
-  FiPlus,
-  FiRefreshCw,
-  FiUser,
-} from "react-icons/fi";
+
 import TimeSide from "./TimeSide";
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  " Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
 const Projects = () => {
-  const [task, setTask] = useState("");
-  const [note, setNote] = useState("");
+  const [text, setText] = React.useState("");
+  const [todo, setTodo] = React.useState([]);
 
-  const tasks = [];
-  const d = new Date();
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let monthName = months[d.getMonth()];
+  const enterkey = (e) => {
+    if (Number(e.keyCode) === 13) {
+      setText(e.target.value);
+      setdata(e);
+    }
+  };
+  const setdata = (e) => {
+    setTodo([...todo, { title: e.target.value, id: Date.now() }]);
+  };
+  React.useEffect(() => {});
+
   return (
     <div>
       <TimeSide />
       <Box mt={-785} ml={265} bgColor={"#f1f1f1"} h={"80vh"} w={"86%"}>
-        <Box mt={-785} ml={50} bgColor={"white"} h={"80vh"} w={"86%"}>
+        <Box mt={-785} bgColor={"white"} h={"10vh"} w={"100%"}>
           {/*TODO:  import Your Component here */}
-          <Box m={"auto"} justifyContent={"center"} h={"80vh"} w={"70%"}>
-            <HStack mt={20} h={100} w={"100%"}></HStack>
-
-            <HStack
-              mt={2}
-              h={"70px"}
-              w={"100%"}
-              boxShadow={"0 0 10px 0 rgb(37 207 96 / 35%)"}
-              border={"1px solid #4bb064"}
-              borderRadius={10}
-              p={2.5}
-            >
-              <Input
-                placeholder="Select task and project"
-                h={"60px"}
-                w={"30%"}
-                variant="outline"
-                border={"none"}
-                _hover={{ border: "1px solid gray" }}
-                value={task}
-                onChange={(e) => setTask(e.target.value)}
-              ></Input>
-
-         
-              
-           
-              <Button
-                _hover={{ backgroundColor: "#7bca84", color: "white" }}
-                bgColor={"#4bb063"}
-                borderColor={"#49ac61"}
-                color={"white"}
-              >
-                ADD Project
-              </Button>
-            </HStack>
-
-            {tasks.length === 0 ? (
-              <Box>
-                <VStack justifyContent="center">
-                  <Image
-                    h={"300px"}
-                    w={"250px"}
-                    src={
-                      "https://cdn.timecamp.com/res/css/images/timesheet-classic-empty-state.svg"
-                    }
-                  ></Image>
-                  <Box ml={5} mt={-50}>
-                    <Text fontSize={"24px"} fontWeight={500}>
-                      No recent Time entries
-                    </Text>
-                  </Box>
-                  <Box ml={5} mt={"-50px"}>
-                    <Text fontSize={"14px"}>
-                      Seems like you haven’t tracked any time yet
-                    </Text>
-                  </Box>
-                </VStack>
-                <HStack>
-                  <Tooltip
-                    label="new time entry"
-                    placement="bottom-start"
-                    fontSize="md"
-                  >
-                    <IconButton
-                      color={"gray"}
-                      bgColor={"white"}
-                      border={"1px solid gray"}
-                      borderRadius={7}
-                      icon={<FiPlus />}
-                    ></IconButton>
-                  </Tooltip>
-                  <Tooltip
-                    label="entries from previous timesheets"
-                    placement="bottom-start"
-                    fontSize="md"
-                  >
-                    <IconButton
-                      color={"gray"}
-                      bgColor={"white"}
-                      border={"1px solid gray"}
-                      borderRadius={7}
-                      icon={<FiCopy />}
-                    ></IconButton>
-                  </Tooltip>
-                  <Spacer />
-                  <Button bgColor={"#fcbd01"} color={"white"}>
-                    Pending Approval - Reopen
-                  </Button>
-                </HStack>
-              </Box>
-            ) : (
-              <Box></Box>
-            )}
-          </Box>
         </Box>
+        <div style={{ width: "100%", height: "150px" }}>
+          <h1 style={{ fontSize: "20px" }}>Enter Project Detail</h1>
+          <Input
+            placeholder="Select task and project"
+            h={"60px"}
+            w={"30%"}
+            variant="outline"
+            border={"1px solid grey"}
+            _hover={{ border: "1px solid gray" }}
+            onKeyUp={enterkey}
+          ></Input>
+        </div>
+
+        <div
+          style={{
+            width: "100%",
+            height: "600px",
+          }}
+        >
+          {todo?.map((e, index) => (
+            <div
+              style={{
+                width: "50%",
+                heigth: "100px",
+              }}
+              key={e.id}
+            >
+              <div style={{ textAlign: "center" }}>
+                <Text
+                  style={{
+                    fontFamily: "sans-serif,popins",
+                    fontWeight: "500",
+                  }}
+                  as="b"
+                  fontSize="30px"
+                >
+                  Project Name: {e.title}
+                </Text>
+              </div>
+            </div>
+          ))}
+        </div>
       </Box>
     </div>
   );
--- sensitive-thunder-3176/src/Redux/AuthReducer/reducer.js
+++ sensitive-thunder-3176/src/Redux/AuthReducer/reducer.js
@@ -1,12 +1,11 @@
-import { getLocalData ,setdata} from "../../Pages/Utils/Savedata";
+import { getLocalData, setdata } from "../../Pages/Utils/Savedata";
 import * as types from "./Actiontype";
 const intial = {
-  isAuth:false,
-  token: getLocalData("token")||undefined,
+  isAuth: false,
+  token: getLocalData("token") || undefined,
   isLoading: false,
   isError: false,
 };
-// getLocalData("token")!==undefined?true:false,
 
 export const reducer = (state = intial, action) => {
   const { type, payload } = action;
@@ -15,14 +14,14 @@ export const reducer = (state = intial, action) => {
     case types.GET_Login_REQUEST:
       return { ...state, isLoading: true };
     case types.GET_Login_SUCCESS:
-      setdata("token",payload)
-      return { ...state, isLoading: false, isAuth: true, token:payload };
+      setdata("token", payload);
+      return { ...state, isLoading: false, isAuth: true, token: payload };
     case types.GET_Login_FAILURE:
       return { ...state, isError: true };
     case types.GET_Loginout_REQUEST:
       return { ...state, isLoading: true };
     case types.GET_Loginout_SUCCESS:
-      setdata("token",undefined)
+      setdata("token", undefined);
       return { ...state, isLoading: false, isAuth: false, token: "" };
     case types.GET_Loginout_FAILURE:
       return { ...state, isError: true };
